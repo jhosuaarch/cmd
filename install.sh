322a298f1470e021cd9aa3e88f417144f968d6af
Jhosua Saut Maruli <80184758+jhosuaarch@users.noreply.github.com>
2025-03-28 11:47:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -sSL https://gist.githubusercontent.com/jhosuaarch/3fbf0933dee988c1e1fb6a4b928cf338/raw/473fa8085554e1de76bf0e6bdddd0b37b57ef43d/gistfile1.txt -o install.sh
+curl -sSL https://gist.githubusercontent.com/jhosuaarch/3fbf0933dee988c1e1fb6a4b928cf338/raw/473fa8085554e1de76bf0e6bdddd0b37b57ef43d/gistfile1.txt -o run.sh
